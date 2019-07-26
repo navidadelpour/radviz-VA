ee005e40e576d18ade029074f38295a442191e3c
Navid <navidadelpour@gmail.com>
2019-07-27 02:04:40 +0430

    [util]: and fix remove unused headers fetchData

--- src/Utils/fetchData.js
+++ src/Utils/fetchData.js
@@ -4,37 +4,27 @@ const fetchData = (url, config, updateDataset, setLoading) => {
 
   setLoading(true)
   let requestBody = {
-    "dataset": config.datasetName, 
-    "clusteringAlgorithm": config.cluster,
-    "clusterSize": config.numCluster
+    dataset: config.datasetName, 
+    clusteringAlgorithm: config.cluster,
+    clusterSize: config.numClusters
   }
-  fetch(
-    url,
-    {
+  fetch(url, {
       method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
+      headers: new Headers({
           'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrer: 'no-referrer',
+      }),
       body: JSON.stringify(requestBody)
-    }
-  )
+  })
   .then(res => res.json())
-  .then(
-    (result) => {
-      updateDataset(transformResponseToNeededData(result))
-      setLoading(false)
-    },
-    (error) => {
-      setLoading(false)
-      alert(error)
-      console.log(error)
-    }
-  )
+  .then((result) => {
+    updateDataset(transformResponseToNeededData(result))
+    setLoading(false)
+  })
+  .catch((error) => {
+    alert(error)
+    console.log("ajax error:", error)
+    setLoading(false)
+  })
 }
 
 export default fetchData;
