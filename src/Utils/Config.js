2c27a8f5edc9c4fd3858a891e2f71705f82e1b43
Navid <navidadelpour@gmail.com>
2019-07-26 21:06:04 +0430

    [util]: config file for radviz

---
+++ src/Utils/Config.js
@@ -0,0 +1,48 @@
+const getConfig = ({
+  useRepulsion, drawLinks, dotSize, width,
+  selector, dimensions, colorAccessor
+}) => {
+
+  return {
+    zoomFactor: 1,
+    margin: 100,
+    
+    size: width,
+    dotRadius: dotSize,
+    useRepulsion: useRepulsion,
+    drawLinks: drawLinks,
+    
+    el: selector,
+    dimensions: dimensions,
+    colorAccessor: (d) => { return d[colorAccessor]; },
+
+    tooltipFormatter: (d) => {
+      return (
+        tooltip(d, dimensions, colorAccessor)
+      )
+    }
+  }
+}
+
+const tooltip = (data, dimensions, colorAccessor) => `
+  <div class="ui bottom left popup transition visible" style="right: auto; position: absolute; will-change: transform; top: 0px; left: 0px;">
+    <h1 class="ui header">${data[colorAccessor]}</h1>
+    <table class="ui celled collapsing very basic compact table">
+      <thead class="">
+        <tr class="">
+          <th class="">Attribute</th>
+          <th class="">Value</th>
+        </tr>
+      </thead>
+      ${dimensions.map((db) => (
+        `<tr class="">
+          <td class=""><b>${db}</b></td>
+          <td class="">${data[db]}</td>
+        </tr>`
+      ))}
+      
+    </table>
+  </div>`
+
+
+export default getConfig;
