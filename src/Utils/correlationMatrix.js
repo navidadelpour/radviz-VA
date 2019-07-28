49ec27dcb9f981ff777e6ad69d43509025fdc6e0
Navid <navidadelpour@gmail.com>
2019-07-29 02:28:21 +0430

    [util]: correlationMatrix script to handle matrix

---
+++ src/Utils/correlationMatrix.js
@@ -0,0 +1,43 @@
+const tableHead = document.getElementsByTagName("thead")[0]
+const tableBody = document.querySelectorAll("tbody")[0]
+
+const removeChilds = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+const emptyCorrelationMatrixContainer = () => {
+    removeChilds(tableHead)
+    removeChilds(tableBody)
+}
+
+const fillRowByElement = (data, element, parent, fixTo) => {
+    var tr = document.createElement("tr");                
+    for(let i = 0; i < data.length; i++) {
+        var tc = document.createElement(element);        
+        if(fixTo && i > 0) {
+            data[i] = data[i].toFixed(fixTo)
+            let colorValue = Math.pow(data[i], 2) * 255
+            tc.style.backgroundColor = `rgba(${colorValue / 3}, ${colorValue / 3}, ${colorValue}, 1)`
+            tc.style.color = "white"
+        }
+        var text = document.createTextNode(data[i]);  
+        tc.appendChild(text)      
+        tr.appendChild(tc);                             
+    }
+    parent.appendChild(tr)
+}
+
+
+const correlationMatrix = (correlationMatrix, classes) => {
+    emptyCorrelationMatrixContainer()
+    fillRowByElement(["", ...classes], "th", tableHead)
+
+    for(let i = 0; i < classes.length; i++){
+        fillRowByElement([classes[i], ...correlationMatrix[i]], "td", tableBody, 2)
+    }
+}
+
+
+export default correlationMatrix
